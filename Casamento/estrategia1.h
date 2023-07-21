5ea346d3f9f0d7cd1c91afc981c7ff8a2cd60d29
_irijon_ <103136574+nojirilucas@users.noreply.github.com>
2023-07-20 23:59:46 -0300

    Delete Casamento directory

--- Casamento/estrategia1.h
+++
@@ -1,9 +0,0 @@
-#ifndef ESTRATEGIA1_H
-#define ESTRATEGIA1_H
-
-#include "lista.h"
-
-int Forca_bruta(const char *string1, const char *string2);
-void Casamento_forca_bruta(No *cabeca, FILE *output);
-
-#endif
